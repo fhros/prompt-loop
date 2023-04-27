a4042714191fbcc3d1ed236d73b4305503ada89e
fhros <112174015+fhros@users.noreply.github.com>
2023-04-27 09:06:44 +0300

    Add files via upload

---
+++ js/javascript.js
@@ -0,0 +1,19 @@
+var answer = Math.floor(Math.random() * 11);
+var question = prompt("anna numero 1-10");
+
+while(question != answer){
+    question = prompt("väärin meni  !!!")
+}
+
+var a = true
+while(a == true){
+    alert("sait oikein 😭👍(oot nyt jumissa 🤣🤣🤣🤣🤣🤣)")
+    //document.body.style.backgroundImage = "url('dog.gif')" ei toimi :(
+}
+
+//if(question == answer) {
+//    document.querySelector("h1").innerText = "yippee"
+//    alert("yippee")
+//} else {
+//    
+//}
